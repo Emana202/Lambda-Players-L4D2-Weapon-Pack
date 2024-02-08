aaabfc02d514f896e09d1f348a4dfa5a1039c342
YerMash <emana202@yandex.ru>
2024-02-08 23:41:53 +0600

    M60 Drop

--- lua/lambdaplayers/lambda/weapons/l4d2_m60.lua
+++ lua/lambdaplayers/lambda/weapons/l4d2_m60.lua
@@ -1,11 +1,6 @@
 local net = net
 local fireDamageTbl = { 20, 24 }
 
-local function OnM60Reload( self )
-    if self:GetWeaponName() == "l4d2_m60" then return end
-    self:OldReloadWeapon()
-end
-
 table.Merge( _LAMBDAPLAYERSWEAPONS, {
     l4d2_m60 = {
         model = "models/lambdaplayers/weapons/l4d2/w_m60.mdl",
@@ -31,29 +26,19 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
             wepent.L4D2Data.MuzzleFlash = 7
             wepent.L4D2Data.IsReloadable = false
             wepent.L4D2Data.DeploySound = "lambdaplayers/weapons/l4d2/m60/gunother/rifle_deploy_1.mp3"
-            
-            self.OldReloadWeapon = self.ReloadWeapon
-            self.ReloadWeapon = OnM60Reload
 
             LAMBDA_L4D2:InitializeWeapon( self, wepent )
         end,
 
-        OnHolster = function( self, wepent )
-            self.ReloadWeapon = self.OldReloadWeapon
-            self.OldReloadWeapon = nil
+        OnReload = function()
+            return true
         end,
 
         OnAttack = function( self, wepent, target )
-            if self.l_Clip <= 0 then 
-                net.Start( "lambdaplayers_createclientsidedroppedweapon" )
-                    net.WriteEntity( wepent )
-                    net.WriteVector( self:GetForward() * 3000 + self:GetUp() * 2500 )
-                    net.WriteVector( wepent:GetPos() )
-                    net.WriteVector( self:GetPhysColor() )
-                    net.WriteString( "l4d2_m60" )
-                net.Broadcast()    
-
-                self:SimpleTimer( 0, function() self:SwitchToRandomWeapon() end )
+            if self.l_Clip <= 0 then
+                self.l_WeaponUseCooldown = ( CurTime() + 1 )
+                self:DropWeapon()
+                self:SimpleTimer( 0.1, function() self:SwitchToRandomWeapon() end )
             else
                 LAMBDA_L4D2:FireWeapon( self, wepent, target )
             end
